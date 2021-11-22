d3f5a99eac78483f58e4d5db49e2d17278900c3b
Sekou04 <94384273+Sekou04@users.noreply.github.com>
2021-11-22 13:48:30 +0100

    Add files via upload

---
+++ main PENDU 2.0.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+
+ 
+
+  int taille ;
+  
+  int i;
+  
+	printf("bienvenue dans le jeu du pendu\n");
+	
+	printf("saisissez la taille de votre mot:");
+	
+	scanf("%d", &taille);
+  
+  char tab[taille];
+  char sk = 'n';
+  
+  while(sk == 'n' || sk=='N'){
+  
+  for(i=0;i<taille;i++){
+  	getchar();
+  	scanf("%c",&tab[taille]);
+  }
+  printf("votre mot est: ");
+  
+  for(i=0;i<taille;i++){
+  	 
+  	printf("%c", tab[i]);
+  	
+  
+}
+
+
+  char oui='O';
+  char non='N';
+  printf("\netes vous satisfait? (o)ui ou (n)on:");
+  getchar();
+  
+   scanf("%c",&tab[0]);
+   if(sk == 'o' || sk == 'N'){
+   }
+   
+   printf("la partie commence!\n");
+   printf("le mot a trouver est:");
+   for(i=0; i<'O'; i++){
+   }
+   i
+   
+    else{
+   }
+
+    
+  
+   return 0;
+   
+}
+  
+    
+
+
+
